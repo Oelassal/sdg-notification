aec07d69214fedb5895f2ea527532f53c72f4a72
Oelassal <omarhanea2@gmail.com>
2026-03-31 04:22:55 +0200

    Initial commit

---
+++ src/main/java/com/omarelassal/sdgnotification/SdgNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.omarelassal.sdgnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdgNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdgNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omarelassal/sdgnotification/SdgNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarelassal.sdgnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdgNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
